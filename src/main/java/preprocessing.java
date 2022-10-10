f3de112981273e90876ef048c319a9a87489016c
SecondulfoValerio <rauros07@gmail.com>
2022-10-10 16:59:24 +0200

    preprocess class

---
+++ src/main/java/preprocessing.java
@@ -0,0 +1,22 @@
+public class preprocessing {
+
+    public static void preprocess (String doc_in) {  //applies the preprocessing
+
+
+
+
+    }
+
+    public static String textclean(String document_in){ //Convert the input document from ASCII to UNICODE
+
+
+    }
+
+    public static void main(String[] args){
+        String text= "0	The presence of communication amid scientific minds was equally important to the success of the Manhattan Project as scientific intellect was. The only cloud hanging over the impressive achievement of the atomic researchers and engineers is what their success truly meant; hundreds of thousands of innocent lives obliterated.";
+
+        preprocessing.preprocess(text);
+    }
+
+
+}
