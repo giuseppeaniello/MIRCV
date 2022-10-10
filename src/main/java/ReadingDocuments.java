d0af035b669ba2457ec92b1fc858743edaa4b286
giuseppeaniello <giuseppe.aniello98@gmail.com>
2022-10-10 16:58:13 +0200

    reading

--- src/main/java/ReadingDocuments.java
+++ src/main/java/ReadingDocuments.java
@@ -1,23 +1,27 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class ReadingDocuments {
-    public static ArrayList<String[]> tsvr(File test2) {
-        ArrayList<String[]> Data = new ArrayList<>(); //initializing a new ArrayList out of String[]'s
+    public static void readDoc() {
+        File test2 = new File("C:\\Users\\onpep\\Desktop\\InformationRetrivial\\Project\\collection.tsv");
+        ; //initializing a new ArrayList out of String[]'s
         try (BufferedReader TSVReader = new BufferedReader(new FileReader(test2))) {
             String line = null;
             while ((line = TSVReader.readLine()) != null) {
-                String[] lineItems = line.split("\t"); //splitting the line and adding its items in String[]
-                Data.add(lineItems); //adding the splitted line array to the ArrayList
+                int docid = Integer.parseInt(line.split("\\t")[0]);
+                String doc = new String(line.split("\\t")[1].getBytes(),"UTF-8");
+                System.out.println(doc);
             }
         } catch (Exception e) {
             System.out.println("Something went wrong");
         }
-        return Data;
     }
     public static void main(String argv[]){
-        System.out.println("DIOCANE");
+        readDoc();
+
+            //System.out.println( "\\u" + Integer.toHexString('÷' | 0x10000).substring(1) );
     }
 }
