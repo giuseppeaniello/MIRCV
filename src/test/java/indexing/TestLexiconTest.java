0e9d17726714e788d0d9cb6225ecb6b0ac872086
giuseppeaniello <giuseppe.aniello98@gmail.com>
2023-01-24 10:49:47 +0100

    add test

---
+++ src/test/java/indexing/TestLexiconTest.java
@@ -0,0 +1,45 @@
+package indexing;
+
+import junit.framework.TestCase;
+
+public class TestLexiconTest extends TestCase {
+
+    public void testAddElement() {
+    }
+
+    public void testSaveLexiconOnFile() {
+    }
+
+    public void testTransformValueToByte() {
+    }
+
+    public void testTransformByteToValue() {
+    }
+
+    public void testClearLexicon() {
+    }
+
+    public void testReadLexiconLine() {
+    }
+
+    public void testReadTermFromBlock() {
+    }
+
+    public void testMergeBlocks() {
+    }
+
+    public void testDeleteFile() {
+    }
+
+    public void testMergeAllBlocks() {
+    }
+
+    public void testComputeScoresForATermTfidfForUpperBound() {
+    }
+
+    public void testComputeScoresForATermBM25ForUpperBound() {
+    }
+
+    public void testReadAllLexicon() {
+    }
+}
